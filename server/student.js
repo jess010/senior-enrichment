4bdca815a3fa59406437a888cb37253c049a862f
Jess Freasier <jfreasier@gmail.com>
2017-08-23 22:02:54 -0500

    minor edits

--- server/student.js
+++ server/student.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const models = require('../db/models');
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     res.send("hi there!")
 })
 
